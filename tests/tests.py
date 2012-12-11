724e6d9858f87fb3d2dd7c2b3295bdf26ab88f86
mr.Shu <mr@shu.io>
2012-12-11 14:21:29 +0100

    added serious tests

--- tests/tests.py
+++ tests/tests.py
@@ -1,13 +1,25 @@
 import unittest
-import os
+import glob
 
+import sys
+sys.path.append("../")
+
+from pynxc import python_to_nxc
 
 class TextConversion(unittest.TestCase):
 
     def test_directories(self):
-        inputs = os.listdir('./in')
-        outputs = os.listdir('./out')
-
+        inputs = glob.glob('./in/*.py')
+        
+        for input in inputs:
+            nxc_filename = input.replace('.py', '.nxc') 
+            python_to_nxc(input, nxc_filename)
+            
+            testfile = nxc_filename.replace('./in/', './out/')
+            
+            print "Testing %s" % input
+            self.assertEqual(open(nxc_filename, 'r').read(), 
+                                open(testfile, 'r').read())
 
 
 if __name__ == '__main__':
