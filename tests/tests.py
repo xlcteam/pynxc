53899747211d97fdb510489efec97a780df531a1
mr.Shu <mr@shu.io>
2012-12-12 22:25:23 +0100

    more tests fix

--- tests/tests.py
+++ tests/tests.py
@@ -9,13 +9,13 @@ from pynxc import python_to_nxc
 class TextConversion(unittest.TestCase):
 
     def test_directories(self):
-        inputs = glob.glob('./in/*.py')
+        inputs = glob.glob('tests/in/*.py')
         
         for input in inputs:
             nxc_filename = input.replace('.py', '.nxc') 
             python_to_nxc(input, nxc_filename)
             
-            testfile = nxc_filename.replace('./in/', './out/')
+            testfile = nxc_filename.replace('tests/in/', 'tests/out/')
             
             print("Testing %s" % input)
             self.assertEqual(open(nxc_filename, 'r').read(), 
