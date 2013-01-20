d51ba480c72c2bac9223a75a4489664d7d0c934c
mr.Shu <mr@shu.io>
2013-01-20 20:11:08 +0100

    updated MANIFEST.in

--- setup.py
+++ setup.py
@@ -1,20 +1,22 @@
 #!/usr/bin/env python
+# -*- coding: utf8 -*-
 
 from distutils.core import setup
 
 setup(name='pynxc',
-      version='0.1.7',
-      description='A Python to NXC Converter for programming'
-                    'LEGO MINDSTORMS Robots',
-      author='Brian Blais',
-      author_email='bblais@bryant.edu',
-      maintainer='Marek Šuppa',
-      maintainer_url='marek@suppa.sk',
-      url='https://github.com/xlcteam/pynxc',
-      packages=['pynxc'],
-      entry_points = {
-          'console_scripts': [
-            'pynxc = pynxc.main'
-          ]
-      }
+    version='0.1.7',
+    description='A Python to NXC Converter for programming'
+                  'LEGO MINDSTORMS Robots',
+    author='Brian Blais',
+    author_email='bblais@bryant.edu',
+    maintainer='Marek Šuppa',
+    maintainer_email='marek@suppa.sk',
+    url='https://github.com/xlcteam/pynxc',
+    packages=['pynxc'],
+    include_package_data=True,
+    entry_points = {
+        'console_scripts': [
+          'pynxc = pynxc.main'
+        ]
+    }
 )
