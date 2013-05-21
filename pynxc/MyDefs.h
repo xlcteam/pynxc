165d3c5233e53eadade6a9dda4757ea741f224cb
mr.Shu <mr@shu.io>
2013-05-21 23:37:11 +0200

    Added off function
    While having in mind the need for a new API I decided to implement the
    off function fairly simply in this way.
    
    This should fix #2

--- pynxc/MyDefs.h
+++ pynxc/MyDefs.h
@@ -12,10 +12,10 @@
 int SensorTypes[]={0,0,0,0};
 
 inline int SensorVal(int s_) {
-    
+
     s_=s_-1;
     int t_ = SensorTypes[s_];
-    
+
     if (t_==None) {
         return -1;
     } else if (t_==ULTRASONIC) {
@@ -24,24 +24,24 @@ inline int SensorVal(int s_) {
     } else if (t_ == COMPASS) {
         return SensorHTCompass(s_);
     } else {
-        return Sensor(s_); 
+        return Sensor(s_);
     }
-    
+
 }
 
 void DefineSensors(int s1,int s2,int s3,int s4) {
-    
+
     int sa[];
     int i;
-    
+
     ArrayInit(sa,0,4);
     sa[0]=s1;
     sa[1]=s2;
     sa[2]=s3;
     sa[3]=s4;
-    
+
     for (i=0; i<4; i++) {
-    
+
         if (sa[i]==None) {
             SensorTypes[i]=None;
         } else if (sa[i]==ULTRASONIC) {
@@ -63,13 +63,13 @@ void DefineSensors(int s1,int s2,int s3,int s4) {
     }
 }
 
-void set_sensor(int port, int type) 
+void set_sensor(int port, int type)
 {
     SensorTypes[port] = type;
     DefineSensors(SensorTypes[0], SensorTypes[1], SensorTypes[2], SensorTypes[3]);
 }
 
-void set_sensors(int s1, int s2, int s3, int s4) 
+void set_sensors(int s1, int s2, int s3, int s4)
 {
     DefineSensors(s1, s2, s3, s4);
 }
@@ -79,4 +79,9 @@ inline int sensor(int port)
     return SensorVal(port);
 }
 
-    
+inline int off(int port = 6)
+{
+    return Off(port);
+}
+
+
