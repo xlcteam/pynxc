205058071453922477ede60d0f541d1baa3ae813
Shane Volpe <shanevolpe@gmail.com>
2013-11-28 04:32:11 -0500

    Added missing data_files to setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,11 @@ setup(name='pynxc',
     maintainer_email='marek@suppa.sk',
     url='https://github.com/xlcteam/pynxc',
     packages=['pynxc'],
-    include_package_data=True,
+    #include_package_data=True,
+
+    data_files=[('tests', ['pynxc/tests/tests.py']), 
+		('', ['pynxc/defs.h'])],
+
     entry_points = {
         'console_scripts': [
           'pynxc = pynxc:main'
